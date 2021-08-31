d5a8f5367fe7fe4c134d2d52e47b646aa6efce5c
Santy <santi.codev@gmail.com>
2021-08-31 14:48:15 +0200

    DEV: Creacion del proyecto

---
+++ src/main/java/com/santycodev/springdatajpa/SpringdatajpaApplication.java
@@ -0,0 +1,13 @@
+package com.santycodev.springdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatajpaApplication.class, args);
+	}
+
+}
